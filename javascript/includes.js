df1c1ca5adf9f1ccf8501a38d017d74c5e3c86b7
SimonCoteDuchesne <simoncoteduchesne@gmail.com>
2021-06-14 13:54:45 -0400

    Update 2021
    Updated a bunch of shit and taylored a bit toward education instead of video games

--- javascript/includes.js
+++ javascript/includes.js
@@ -32,10 +32,10 @@ function addFooterFr()
 	footer.innerHTML =
 		"<p id=\"copyright\">&copy; Simon Côté-Duchesne</p>"
 		+ "<p id=\"back-top\"><a href=\"#\" class=\"back-to-top\">Retour vers le haut</a></p>"
-		+ "<a id=\"social\" href=\"https://www.facebook.com/8arbe8leue\" target=\"_blank\"><img src=\"images/facebook.png\" alt=\"facebook\"></a>"
+		//+ "<a id=\"social\" href=\"https://www.facebook.com/8arbe8leue\" target=\"_blank\"><img src=\"images/facebook.png\" alt=\"facebook\"></a>"
 		+ "<a id=\"social\" href=\"https://www.linkedin.com/in/simoncoteduchesne/\" target=\"_blank\"><img src=\"images/linkedin.png\" alt=\"linkedin\"></a>"
-		+ "<a id=\"twitter\" href=\"https://twitter.com/Simon_CoteDuch\" target=\"_blank\"><img src=\"images/twitter.png\" alt=\"twitter\"></a>"
-		+ "<a id=\"social\" href=\"https://github.com/SimonCoteDuchesne\" target=\"_blank\"><img src=\"images/github.png\" alt=\"github\"></a>"
+		//+ "<a id=\"twitter\" href=\"https://twitter.com/Simon_CoteDuch\" target=\"_blank\"><img src=\"images/twitter.png\" alt=\"twitter\"></a>"
+		//+ "<a id=\"social\" href=\"https://github.com/SimonCoteDuchesne\" target=\"_blank\"><img src=\"images/github.png\" alt=\"github\"></a>"
 		+ "<p id=\"menu\"><br><a href=\"index.html\">Accueil</a> | <a href=\"experiences.html\">Expérience</a> | <a href=\"curriculumvitae.html\">CV</a> | <a href=\"contact.html\">Contact</a></p>"
 }
 
@@ -45,10 +45,10 @@ function addFooterEn()
 	footerEn.innerHTML =
 		"<p id=\"copyright\">&copy; Simon Côté-Duchesne</p>"
 		+ "<p id=\"back-top\"><a href=\"#\" class=\"back-to-top\">Back to top</a></p>"
-		+ "<a id=\"social\" href=\"https://www.facebook.com/8arbe8leue\" target=\"_blank\"><img src=\"images/facebook.png\" alt=\"facebook\"></a>"
+		//+ "<a id=\"social\" href=\"https://www.facebook.com/8arbe8leue\" target=\"_blank\"><img src=\"images/facebook.png\" alt=\"facebook\"></a>"
 		+ "<a id=\"social\" href=\"https://www.linkedin.com/in/simoncoteduchesne/\" target=\"_blank\"><img src=\"images/linkedin.png\" alt=\"linkedin\"></a>"
-		+ "<a id=\"twitter\" href=\"https://twitter.com/Simon_CoteDuch\" target=\"_blank\"><img src=\"images/twitter.png\" alt=\"twitter\"></a>"
-		+ "<a id=\"social\" href=\"https://github.com/SimonCoteDuchesne\" target=\"_blank\"><img src=\"images/github.png\" alt=\"github\"></a>"
+		//+ "<a id=\"twitter\" href=\"https://twitter.com/Simon_CoteDuch\" target=\"_blank\"><img src=\"images/twitter.png\" alt=\"twitter\"></a>"
+		//+ "<a id=\"social\" href=\"https://github.com/SimonCoteDuchesne\" target=\"_blank\"><img src=\"images/github.png\" alt=\"github\"></a>"
 		+ "<p id=\"menu\"><br><a href=\"index_En.html\">Home</a> | <a href=\"experiences_En.html\">Experience</a> | <a href=\"curriculumvitae_En.html\">CV</a> | <a href=\"contact_En.html\">Contact</a></p>"
 }
 
